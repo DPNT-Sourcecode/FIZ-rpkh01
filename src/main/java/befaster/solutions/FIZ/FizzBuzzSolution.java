9db10da904d588904084fcc3f0cbb94727506887
sbx_user1060 <sbx_user1060@ip-10-128-104-100.eu-west-2.compute.internal>
2018-09-18 14:37:55 +0000

    Tue Sep 18 14:37:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,5 +1,8 @@
 package befaster.solutions.FIZ;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
@@ -11,6 +14,9 @@ public class FizzBuzzSolution {
     		builder.append("fizz ");
     	}
     	if (number % 5 == 0 || number.toString().contains("5")) {
+    		builder.append("buzz ");    		
+    	}
+    	if (number > 10 && checkDeluxe(number)) {
     		builder.append("buzz");    		
     	}
     	if(builder.length() == 0) {
@@ -19,6 +25,13 @@ public class FizzBuzzSolution {
     		result = builder.toString().trim();
     	}
     	return result;
+    }
+    
+    private Boolean checkDeluxe(Integer number) {
+    	
+    	Map<CharSequence,Integer> map = new HashMap<>();
+    	for parameter.in
+//    	chars().forEach( map.put(i, 1));
     }
 
 }
